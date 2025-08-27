98cc8701d7bbc8d1e462c187240a254921b385f7
Basil648 <basilshajan666@gmail.com>
2025-08-27 12:07:18 +0530

    fixed a crucial mistake

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Header'
 import Navbar from './Navbar'
 import { Outlet } from 'react-router-dom'
-import Footer from './footer'
+import Footer from './Footer'
 
 function Layout() {
     return (
